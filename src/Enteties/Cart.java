42268af7c2993a09131befbe57ff751936dc3fba
Kamohelo Ntimane <ntimanevincent8@gmail.com>
2025-07-10 13:31:53 +0200

    Created orders and DefaultOrders interface and Class

--- src/Enteties/Cart.java
+++ src/Enteties/Cart.java
@@ -1,5 +1,10 @@
 package Enteties;
 
-public class Cart {
+public interface Cart {
+	
+	boolean isEmpty();
+	void addProduct(Product productById);
+	Product[] getProducts();
+	void clear();
 
 }
--- src/Enteties/Impl/DefaultCart.java
+++ src/Enteties/Impl/DefaultCart.java
@@ -1,49 +1,91 @@
 package Enteties.Impl;
 
-public class DefaultCart {
+import java.util.Arrays;
 
-private static int userCounter = 0;
-	
-	private int id;
-	private String firstName;
-	private String lastName;
-	private String password;
-	private String email;
+import Enteties.Cart;
+import Enteties.Product;
+
+public class DefaultCart implements Cart {
+
+	private static final int DEFAULT_PRODUCT_CAPACITY = 10; //Only keeps a number of products
+	private Product[] products; //Stores different types of products
+	private int lastIndex;
 	
 	{
-		id = ++userCounter;
+		products = new Product[DEFAULT_PRODUCT_CAPACITY];
 	}
 	
-	//Constructor
-	DefaultCart(){
+	@Override
+	public boolean isEmpty() {
+		if (products == null || products.length == 0) {
+			return true;
+		}
 		
+		for (Product product : products) {
+			if (product != null) {
+				return false;
+			}
+		}
+		return true;
 	}
 	
-	DefaultCart(String firstName, String lastName, String password, String email){
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.password = password;
-		this.email = email;
+	@Override
+	public void addProduct(Product product) {
+		if (product == null) {
+			return;
+		}
+		if (products.length <= lastIndex) {
+			products = Arrays.copyOf(products, products.length << 1);
+			//Arrays.copyOf(products, products.length << 1):- 
+			//Creates a new array with double the old capacity, 
+			//and copies all existing products with it.
+			
+			//-products.length << 1:- This is a bitwise left shift, 
+			//equivalent to multiplying by 2. So the array capacity doubles
+			
+		}
+		products[lastIndex++] = product;
 	}
 	
-	//Setters and Getters
 	
-	public String getFirstName() {
-		return firstName;
+	//Purpose- Return a new array of Product object, Containing only the non-null products
+	//From existing array called products.
+	//Original products array may contain null elements. this method filters them out.
+	@Override
+	public Product[] getProducts() {
+		
+		//nonnullProductsAmount holds the count of all non-null Product objects in array
+		int nonNullProductsAmount = 0; //Initializes a counter to zero
+		for (Product product : products ) { //Iterates through the products array
+			if (product != null) { 
+				nonNullProductsAmount++; //Every element that is not null, increase the counter
+			}
+		}
+		
+		
+		//Creates a new array nonNullProducts- with length equal to the number of non-null products found.
+		Product[] nonNullProducts = new Product[nonNullProductsAmount];
+		int index = 0;
+		for (Product product : products) {
+			if (product != null) {
+				nonNullProducts[index++] = product;
+			}
+		}
+		return nonNullProducts;
+	}
+	
+	@Override
+	public void clear() {
+		products = new Product[DEFAULT_PRODUCT_CAPACITY];
 	}
 	
 	
-	public String getLastName() {
-		return lastName;
-	}
 	
 	
-	public String getPassword() {
-		return password;
-	}
 	
-	public String getEmail() {
-		return email;
-	}
+	
+	
+	
+	
 	
 }
