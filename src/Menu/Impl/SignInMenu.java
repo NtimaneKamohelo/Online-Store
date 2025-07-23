cd204b99cadd803f918016d7b09507b9b1dbf0d8
Kamohelo Ntimane <ntimanevincent8@gmail.com>
2025-07-23 13:29:09 +0200

    Created a SignOutMenu class where the user can sign out from the application

--- src/Menu/Impl/SignInMenu.java
+++ src/Menu/Impl/SignInMenu.java
@@ -1,5 +1,47 @@
 package Menu.impl;
 
-public class SignInMenu {
+import java.util.Scanner;
 
+import Enteties.User;
+import Menu.menu;
+import configs.ApplicationContext;
+import services.UserManagementService;
+import services.impl.DefaultUserManagementService;
+
+public class SignInMenu implements menu{
+	
+	private UserManagementService userManagementService;
+	private ApplicationContext context;
+	
+	{
+		context = ApplicationContext.getInstance();
+		userManagementService = DefaultUserManagementService.getInstance();
+	}
+	
+	@Override
+	public void start() {
+		printMenuHeader();
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Please, enter your email: ");
+		String userEmail = sc.next();
+		
+		System.out.print("Please, enter your password: ");
+		String userPassword = sc.next();
+		
+		User user = userManagementService.getUserByEmail(userEmail);
+		if (user != null && user.getPassword().equals(userPassword)) {
+			System.out.printf("Glad to see you back %s %s", user.getFirstName(), 
+					user.getLastName() + System.lineSeparator());
+			context.setLoggedInUser(user);
+		} else {
+			System.out.println("Unfortunately, such login and password doesn't exist.");
+		}	
+	}
+	
+	@Override
+	public void printMenuHeader() {
+		System.out.println("***** Sign In *****");
+	}
+	
 }
